5d690155a46f66517e097320822c06834d5cd5a8
Pedro Dantas <pedrood1232@gmail.com>
2022-03-03 08:44:32 -0300

    feat: adicionando função de escrita no chat

---
+++ main.js
@@ -0,0 +1,14 @@
+const userName = "Jujuba louco";
+
+document.getElementById('chat').addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    let messageDiv = document.getElementById('message');
+
+    document.getElementById('messages').insertAdjacentHTML('beforeend', sendMessage(messageDiv.value));
+    messageDiv.value = '';
+});
+
+function sendMessage(message) {
+    return `<div>${userName}: ${message}</div>`
+}
